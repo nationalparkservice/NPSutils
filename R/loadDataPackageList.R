3b1c70495b54059587003b501f55f6239428cbe9
Patterson <JuddPatterson@nps.gov>
2021-10-14 17:50:07 -0400

    spelling and spacing updates

--- R/loadDataPackageList.R
+++ R/loadDataPackageList.R
@@ -1,4 +1,4 @@
-#' Read contents of data packge file and construct a vector with the list of data frames to be created.
+#' Read contents of data package file and construct a vector with the list of data frames to be created.
 #' For file geodatabases this will only extract spatial data features.
 #'
 #' \code{loadDataPackageList} reads the data file from a package and loads it into a data frame.
@@ -6,14 +6,14 @@
 #' @param HoldingID is a 6-7 digit number corresponding to the holding ID of the data package zip file.
 #' @param dataFormat is a character value indicating the format of the data set(s) within the data package. Currently
 #' allowable options are:
-#' * "csv" for comma seperated value text files
+#' * "csv" for comma separated value text files
 #' * "gdb" for file geodatabases
 #' @param metadataFormat is a character value indicating the format of the metadata file within the data package.
 #' Currently allowable options are:
 #' * "eml" for eml-compliant xml metadata files
 #' * "fgdc" for FGDC-compliant xml metadata files
 #'
-#' @return one or more data frames contained within the data package to the global enviroment.
+#' @return one or more data frames contained within the data package to the global environment.
 #'
 #' @examples
 #'
@@ -25,15 +25,11 @@ loadDataPackageList <-function(HoldingID,dataFormat,metadataFormat){
   DataPackageFilename<-paste(DataPackageDirectory,"/",HoldingID,".zip",sep="")
 
   if (dataFormat=="csv" & metadataFormat=="eml") {
-
     fileList<-unzip(DataPackageFilename,list=TRUE)
     return(fileList)
-
-
+    
   } else if (dataFormat=="gdb" & metadataFormat=="fgdc") {
-
     # And now working with the file geodatabase
-
     gdbfile <- list.dirs(path = DataPackageDirectory, full.names = TRUE, recursive = FALSE)
     fGDB <- paste0(getwd(),"/",gdbfile)
     datasets<-sf::st_layers(fGDB,do_count=TRUE)
@@ -49,9 +45,7 @@ loadDataPackageList <-function(HoldingID,dataFormat,metadataFormat){
         datasetList<-rbind.data.frame(datasetList,temp2,stringsAsFactors = FALSE)
         }
     }
-
     return(datasetList)
-
 
   } else {
     print ("data/metadata format combination not supported")
