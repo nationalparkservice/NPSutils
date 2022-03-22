424a6b74464df1ba40976cf41b2cbd50e327e47f
Baker <robert_baker@nps.gov>
2022-03-22 12:20:20 -0600

    Changed download location to /data instead of data/raw/. Downloadlink is now directly accessed through metadata instead of constructed from different pieces of metadata.

--- R/getDataPackage.R
+++ R/getDataPackage.R
@@ -1,6 +1,6 @@
 #' Retrieve public digital data package holding from Data Store.
 #'
-#' \code{getDataPackage} downloads a data package from data store, copies it to the /data/raw directory, and unzips it into a subdirectory with the "HoldingID" name.
+#' \code{getDataPackage} downloads a data package from data store, copies it to the /data directory, and unzips it into a sub-directory of /data with the "HoldingID" name.
 #'
 #'
 #' @param ReferenceID is a 6-7 digit number corresponding to the reference ID of the data package.
@@ -15,12 +15,8 @@ getDataPackage<-function(ReferenceID,Secure=FALSE){
   if (!file.exists("data")) {
     dir.create("data")
   }
-  
-  if (!file.exists("data/raw")) {
-    dir.create("data/raw")
-  }
 
-  DestinationDirectory<-paste("data/raw/",ReferenceID,sep="")
+  DestinationDirectory<-paste("data/",ReferenceID,sep="")
   if (!file.exists(DestinationDirectory)) {
     dir.create(DestinationDirectory)
   }
@@ -29,46 +25,48 @@ getDataPackage<-function(ReferenceID,Secure=FALSE){
     # get HoldingID from the ReferenceID - defaults to the first holding
     RestHoldingInfoURL <- paste0('https://irmaservices.nps.gov/datastore/v4/rest/reference/',ReferenceID,'/DigitalFiles')
     xml <- httr::content(httr::GET(RestHoldingInfoURL))
-    HoldingID <- xml[[1]]$resourceId
+    RestDownloadURL<-xml[[1]]$downloadLink
+    #HoldingID <- xml[[1]]$resourceId
     DigitalFileType <- xml[[1]]$extension
-    if (HoldingID > 0) {
-      RestDownladURL<-paste0('https://irmaservices.nps.gov/datastore-secure/v4/rest/DownloadFile/',HoldingID)
-    } else {
-      stop("An error occurred. A HoldingID could not be found for this reference.")
-    }
-    
-  } else if (Secure=="FALSE") {
+    #if (HoldingID > 0) {
+    #  RestDownladURL<-paste0('https://irmaservices.nps.gov/datastore-secure/v4/rest/DownloadFile/',HoldingID)
+    #} else {
+    #  stop("An error occurred. A HoldingID could not be found for this reference.")
+    #}
+  
+}   else if (Secure=="FALSE"){
     # get the HoldingID from the ReferenceID - defaults to the first holding
     RestHoldingInfoURL <- paste0('https://irmaservices.nps.gov/datastore/v4/rest/reference/',ReferenceID,'/DigitalFiles')
     xml <- httr::content(httr::GET(RestHoldingInfoURL))
-    HoldingID <- xml[[1]]$resourceId
+    RestDownloadURL<-xml[[1]]$downloadLink
+    #HoldingID <- xml[[1]]$resourceId
     DigitalFileType <- xml[[1]]$extension
-    if (HoldingID > 0) {
-      RestDownladURL<-paste0('https://irmaservices.nps.gov/datastore/v4/rest/DownloadFile/',HoldingID)
-    } else {
-      stop("An error occurred. A HoldingID could not be found for this reference.")
-    }
-    
+    #if (HoldingID > 0) {
+    #  RestDownloadURL<-paste0('https://irmaservices.nps.gov/datastore/v4/rest/DownloadFile/',HoldingID)
+    #else {
+    #  stop("An error occurred. A HoldingID could not be found for this reference.")
+    #}
   }
-
+  
   # download the data package from Data Store into its own directory
-  DestinationFilename<-paste(DestinationDirectory,"/",ReferenceID,".", DigitalFileType ,sep="")
-  download.file(RestDownladURL,DestinationFilename,quiet=FALSE, mode="wb")
+  DestinationFilename<-paste("data/",ReferenceID,".", DigitalFileType ,sep="")
+  #DestinationFilename<-paste(DestinationDirectory,"/",ReferenceID,".", DigitalFileType ,sep="")
+  download.file(RestDownloadURL,DestinationFilename,quiet=FALSE, mode="wb")
 
   # unzip data package
   # check to see if the downloaded file is a zip
-  if (tools::file_ext(DestinationFilename) == "zip") {
+  if(tools::file_ext(DestinationFilename) == "zip"){
     unzip(DestinationFilename, exdir = DestinationDirectory)
   }
   else {
     rlang::inform("File was not a zip file and could not be unzipped")
   }
-  
+    
   # check to see that the zip was downloaded and unzipped
   # this might be worth improving in the future, but looks for >1 files in the folder
   if (length(list.files(DestinationDirectory, include.dirs = FALSE)) > 1) {
     rlang::inform(paste0("Download and unzipping of reference ",ReferenceID," succeeded"))
-  } else {
+    } else { 
     rlang::inform(paste0("Download and unzipping of reference ",ReferenceID," failed"))
   }
 }
