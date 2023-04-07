870be630b91daa6505836cc6f32aadc26804761a
Rob Baker <robert_baker@nps.gov>
2023-04-07 14:16:39 -0600

    add code chunk to test for new version - but in DataStore prevents this from working but it can be activated when DS2.9 is pushed (May 2023?)

--- R/getDataPackage.R
+++ R/getDataPackage.R
@@ -29,36 +29,72 @@ get_data_packages <- function(reference_id,
                               secure = FALSE,
                               path=here::here(),
                               force = FALSE) {
-  
   #capture original working directory
   orig_wd <- getwd()
-  #return the directory where /data is written.
-  on.exit(return(getwd()))
   #set directory back to original working directory on exit.
   on.exit(setwd(orig_wd), add=TRUE)
-  
   #set wd to path; defaults to wd. 
   setwd(path)
-  
   #create "data" directory, if necessary:
   if (!file.exists("data")) {
     dir.create("data")
   }
-  
   #Secure route (needs further testing).
   if (toupper(secure) == "TRUE") {
     for(i in seq_along(reference_id)){
       #create a package-specific sub-directory within "data", if necessary:
-      destination_dir <- paste("data/", reference_id[i], sep = "")
-      
-      
+      destination_dir <- paste("data/",
+                               reference_id[i],
+                               sep = "")
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+# To impliment after DataStore patch 2.9 is pushed ----
+      #check for newer version:
+      #if(force == FALSE){
+      #  url <- paste0(
+      #    "https://irmaservices.nps.gov/datastore-secure/v5/rest/ReferenceCodeSearch?q=",
+      #    reference_id[i])
+        #api call to see if ref exists
+      #  test_req <- httr::GET(url, httr::authenticate(":", ":", "ntlm"))
+      #  status_code <- httr::stop_for_status(test_req)$status_code
+      #  #if API call fails, alert user and remind them to log on to VPN:
+      #  if(!status_code == 200){
+      #    stop("ERROR: DataStore connection failed. Are you logged in to the VPN?\n")
+      #  }
+        #get version info:
+      #  ref_data <- jsonlite::fromJSON(
+      #    httr::content(
+      #      test_req, "text"))
+      # version <-ref_data$newestVersion
+        #date <- stringr::sub_str(ref_data$dateOfIssue, 1, 10)
+      #  if(!is.na(version)){
+      #    cat("There is a newer version of ",
+      #        crayon::blue$bold(reference_id[i]),
+      #        " available.\n",
+      #        sep = "")
+      #    cat("The newest version, ", 
+      #          crayon::bold$blue(version)),
+      #        " was uploaded on ",
+      #          crayon::bold$blue(date),
+      #        ".\n",
+      #        sep = "")
+      #    cat("Would you like to download the newest version instead?\n\n")
+      #    var2 <- readline(prompt = "1: Yes\n2: No\n")
+      #    if(var1 == 1){
+      #      reference_id[i] <- version
+      #    }
+      #  }
+      #}     
       #if the directory already exists, prompt user to overwrite:
+# End version testing code section----
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
       if (file.exists(destination_dir) & force == FALSE){
         cat("The directory ",
             crayon::blue$bold(destination_dir),
             " already exists.\n",
             sep = "")
-        cat("Would you like to write over the existing directory and its contents?\n\n")
+        cat("Write over the existing directory and its contents?\n\n")
         var1 <- readline(prompt = "1: Yes\n2: No\n")
         if(var1 == 2){
           cat("The original directory ",
@@ -92,9 +128,11 @@ get_data_packages <- function(reference_id,
             cat(crayon::red$bold(
               "ERROR: You do not have permissions to access ",
               crayon::blue$bold(reference_id[i]),
-              ".", "\n", sep=""))
+              ".\n", sep = ""))
             cat("Try logging on to the NPS VPN before running ",
-                crayon::green$bold("get_data_package()"), ".", "\n", sep="")
+                crayon::green$bold("get_data_package()"),
+                ".\n",
+                sep = "")
             cat(crayon::red$bold("Function terminated.\n"))
             stop()
           }
@@ -114,7 +152,9 @@ get_data_packages <- function(reference_id,
                                overwrite = TRUE),
               httr::authenticate(":", ":", "ntlm")))))
         if(force == FALSE){
-          cat("writing: ", crayon::blue$bold(download_file_path), ".\n", sep="")
+          cat("writing: ",
+              crayon::blue$bold(download_file_path),
+              ".\n", sep="")
         }    
         #test for .zip; if found extract files and delete .zip file.
         if (tools::file_ext(tolower(download_filename)) == "zip") {
@@ -130,16 +170,22 @@ get_data_packages <- function(reference_id,
             },
             warning = function(w){
               if(stringr::str_detect(w$message, "-1") & force == FALSE){
-                cat(crayon::red$bold("The .zip file appears empty. Are you sure you have permissions to access this file?\n"))
+                cat("The .zip file appears empty. ",
+                    crayon::red$bold("Are you sure you have permissions to access this file?\n"),
+                    sep = "")
               }
               if(stringr::str_detect(w$message, "3") & force == FALSE){
-                cat(crayon::red$bold("There was an undiagnosed problem with the .zip file. Please double check your results.\n"))
+                cat("There was an undiagnosed problem with the .zip file. ",
+                    crayon::red$bold("Please double check your results.\n"),
+                    sep = "")
               }
             },
             finally = {
               if(force == FALSE){
-              cat("unzipping ", crayon::blue$bold(download_filename),
-                  ".\n", sep="")
+              cat("unzipping ", 
+                  crayon::blue$bold(download_filename),
+                  ".\n",
+                  sep="")
               }
               #remove .zip after extracting
               file.remove(paste0("data/", reference_id[i], "/",
@@ -166,7 +212,7 @@ get_data_packages <- function(reference_id,
             crayon::blue$bold(destination_dir),
             " already exists.\n",
             sep = "")
-        cat("Would you like to write over the existing directory and its contents?\n\n")
+        cat("Write over the existing directory and its contents?\n\n")
         var1 <- readline(prompt = "1: Yes\n2: No\n")
         if(var1 == 2){
           cat("The original directory ",
@@ -197,7 +243,6 @@ get_data_packages <- function(reference_id,
             " and set ", crayon::bold$blue("secure=TRUE"), ".\n", sep="")
         cat("Don't forget to log on to the VPN!\n")
       }
-      
       #download all files in reference:
       if(is.null(xml$message)){
         for(j in seq_along(xml)){
@@ -212,7 +257,9 @@ get_data_packages <- function(reference_id,
                       quiet=TRUE, 
                       mode="wb")
           if(force == FALSE){
-            cat("writing: ", crayon::blue$bold(download_file_path), "\n", sep="")
+            cat("writing: ",
+                crayon::blue$bold(download_file_path),
+                "\n", sep="")
           }        
           # check to see if the downloaded file is a zip; unzip.
           if (tools::file_ext(tolower(download_filename)) == "zip") {
@@ -221,7 +268,10 @@ get_data_packages <- function(reference_id,
                                     download_filename),
                      exdir = paste0("data\\", reference_id[i]))
           #delete .zip file
-            file.remove(paste0("data/", reference_id[i], "/", download_filename))
+            file.remove(paste0("data/",
+                               reference_id[i],
+                               "/",
+                               download_filename))
             if(force == FALSE){
               cat("unzipping",
                   crayon::blue$bold(download_filename),
