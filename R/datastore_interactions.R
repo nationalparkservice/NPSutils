1974571380950a54c2a2614ff350fc66862303de
Rob Baker <robert_baker@nps.gov>
2024-05-13 21:16:55 -0600

    check_ref_exists no defaults to secure = TRUE but allows secure = FALSE

--- R/datastore_interactions.R
+++ R/datastore_interactions.R
@@ -14,18 +14,29 @@
 #' \dontrun{
 #' check_ref_exists(1234567)
 #' }
-check_ref_exists <- function (reference_id) {
-  #construct request url:
-  url <- paste0(.ds_secure_api(), "ReferenceCodeSearch?q=", reference_id[i])
+check_ref_exists <- function (reference_id, secure = TRUE) {
   
-  #api call to see if ref exists
-  test_req <- httr::GET(url, httr::authenticate(":", ":", "ntlm"))
-  status_code <- httr::stop_for_status(test_req)$status_code
-  #if API call fails, alert user and remind them to log on to VPN:
-  if(!status_code == 200){
-    stop("DataStore connection failed. Are you logged in to the VPN?\n")
+  test_req <- NULL
+  if (secure == FALSE) {
+    #using secure api should be an option though...
+    url <- paste0(.ds_api(), "ReferenceCodeSearch?q=", reference_id)
+    test_req <- httr::GET(url)
+    status_code <- httr::stop_for_status(test_req)$status_code
+    #if API call fails, alert user and remind them to log on to VPN:
+    if(!status_code == 200){
+      stop("DataStore connection failed. Try setting secure = TRUE and logging on to the VPN.\n")
+    }
   }
-  #get version info:
+  if (secure == TRUE) {
+    url <- paste0(.ds_secure_api(), "ReferenceCodeSearch?q=", reference_id)
+    test_req <- httr::GET(url, httr::authenticate(":", ":", "ntlm"))
+    status_code <- httr::stop_for_status(test_req)$status_code
+    #if API call fails, alert user and remind them to log on to VPN:
+    if (!status_code == 200) {
+      stop("DataStore connection failed. Are you logged in to the VPN?\n")
+    }
+  }
+  #get API request info:
   ref_data <- jsonlite::fromJSON(httr::content(test_req, "text"))
   
   #if an invalid reference number was supplied (no reference found):
@@ -36,16 +47,34 @@ check_ref_exists <- function (reference_id) {
   }
 }
   
-  #Alert to incorrect (not data package) reference type:
-  ref_type <- ref_data$referenceType
-  if(!identical(ref_type, "Data Package")){
-    cat("Error: reference ", crayon::red$bold(reference_id[i]),
-        " is a ", crayon::red$bold(ref_type),
-        " not a data package.\n", sep ="")
-    cat("Would you like to attempt to download the resource anyway?\n\n")
+check_is_data_package <- function(reference_id) {
+  #using secure api should be an option; can this happen silently?
+  url <- paste0(.ds_api(), "ReferenceCodeSearch?q=", reference_id)
+  
+  #does this really need to authenticate?
+  test_req <- httr::GET(url, httr::authenticate(":", ":", "ntlm"))
+  status_code <- httr::stop_for_status(test_req)$status_code
+  #if API call fails, alert user and remind them to log on to VPN:
+  if(!status_code == 200){
+    stop("DataStore connection failed. Are you logged in to the VPN?\n")
+  }
+  #get reference info:
+  ref_data <- jsonlite::fromJSON(httr::content(test_req, "text"))
+  
+  
 }
 
+
 check_new_version <- function(referenc_id) {
-  url <- paste0(.ds_secure_api(), "ReferenceCodeSearch?q=", reference_id[i])
+  url <- paste0(.ds_secure_api(), "ReferenceCodeSearch?q=", reference_id)
+  test_req <- httr::GET(url, httr::authenticate(":", ":", "ntlm"))
+  status_code <- httr::stop_for_status(test_req)$status_code
+  #if API call fails, alert user and remind them to log on to VPN:
+  if(!status_code == 200){
+    stop("DataStore connection failed. Are you logged in to the VPN?\n")
+  }
+  #get version info:
+  ref_data <- jsonlite::fromJSON(httr::content(test_req, "text"))
+  ref_type <- ref_data$referenceType
   
 }
