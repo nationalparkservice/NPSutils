bbb3ac345b2c5bc743ac44f0abf31ecc76427720
Rob Baker <robert_baker@nps.gov>
2024-05-13 21:45:25 -0600

    add function checke_new_version

--- R/datastore_interactions.R
+++ R/datastore_interactions.R
@@ -51,7 +51,7 @@ check_ref_exists <- function (reference_id, secure = TRUE) {
   
 #' Check whether reference is a data package
 #' 
-#' @description first runs `check_reference_exists()`. Assuming the reference exists, the function test whether the reference is a data package or not. If it is a data package, the value "TRUE" is returned. If it is not a data package, the value FALSE is returned. The default setting assumes the user is logged on to the VPN (secure = TRUE), but this can be set to secure = FALSE for non-NPS users.
+#' @description The function tests whether a valid DataStore reference is a data package or not. If it is a data package, the value "TRUE" is returned. If it is not a data package, the value FALSE is returned. The default setting assumes the user is logged on to the VPN (secure = TRUE), but this can be set to secure = FALSE for non-NPS users. This is a relatively simple helper function that will not test whether a reference exists and may produce unexpected errors if the reference does not exist or is not accessible. You are advised to run `check_reference_exists()` prior to using this function.
 #'
 #' @inheritParams check_ref_exists
 #'
@@ -64,16 +64,8 @@ check_ref_exists <- function (reference_id, secure = TRUE) {
 #' check_is_data_package(1234567, secure = FALSE)
 #' }
 check_is_data_package <- function(reference_id, secure = TRUE) {
-  #check whether reference exists:
-  good_ref_id <- check_ref_exists(reference_id = reference_id,
-                                  secure = secure)
-  if (good_ref_id == FALSE) {
-    stop("Incorrect or unavailable reference id provided.")
-  } 
-  #assuming it does exist:
   test_req <- NULL
   if (secure == FALSE) {
-    #using secure api should be an option though...
     url <- paste0(.ds_api(), "ReferenceCodeSearch?q=", reference_id)
     test_req <- httr::GET(url)
     status_code <- httr::stop_for_status(test_req)$status_code
@@ -104,16 +96,53 @@ check_is_data_package <- function(reference_id, secure = TRUE) {
 }
 
 
-check_new_version <- function(referenc_id) {
-  url <- paste0(.ds_secure_api(), "ReferenceCodeSearch?q=", reference_id)
-  test_req <- httr::GET(url, httr::authenticate(":", ":", "ntlm"))
-  status_code <- httr::stop_for_status(test_req)$status_code
-  #if API call fails, alert user and remind them to log on to VPN:
-  if(!status_code == 200){
-    stop("DataStore connection failed. Are you logged in to the VPN?\n")
+#' Checks whether a reference has a more recent version
+#'
+#' @description
+#' This function tests whether an existing and accessible DataStore reference
+#' has a newer version or not. If a newer version exists, the value "TRUE" is returned. If a newer version does not exist, the value FALSE is returned. The default setting assumes the user is logged on to the VPN (secure = TRUE), but this can be set to secure = FALSE for non-NPS users. This is a relatively simple helper function that will not test whether a reference exists and may produce unexpected errors if the reference does not exist or is not accessible. You are advised to run `check_reference_exists()` prior to using this function.
+#' 
+#' @inheritParams check_ref_exists
+#' 
+#' @return Logical. TRUE if a newer version exists, FALSE if a newer version
+#'  does not exist.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' check_new_version(1234567)
+#' check_new_version(1234567, secure = FALSE)
+#' }
+check_new_version <- function(referenc_id, secure = TRUE) {
+  test_req <- NULL
+  if (secure == FALSE) {
+    #using secure api should be an option though...
+    url <- paste0(.ds_api(), "ReferenceCodeSearch?q=", reference_id)
+    test_req <- httr::GET(url)
+    status_code <- httr::stop_for_status(test_req)$status_code
+    #if API call fails, alert user and remind them to log on to VPN:
+    if(!status_code == 200){
+      stop("DataStore connection failed. Try setting secure = TRUE and logging on to the VPN.\n")
+    }
   }
-  #get version info:
+  if (secure == TRUE) {
+    url <- paste0(.ds_secure_api(), "ReferenceCodeSearch?q=", reference_id)
+    test_req <- httr::GET(url, httr::authenticate(":", ":", "ntlm"))
+    status_code <- httr::stop_for_status(test_req)$status_code
+    #if API call fails, alert user and remind them to log on to VPN:
+    if (!status_code == 200) {
+      stop("DataStore connection failed. Are you logged in to the VPN?\n")
+    }
+  }
+  #get request info:
   ref_data <- jsonlite::fromJSON(httr::content(test_req, "text"))
-  ref_type <- ref_data$referenceType
+  #check for a newer version:    
+  version <-ref_data$mostRecentVersion
   
+  #test for newer version:
+  if (!is.na(version)) {
+    return(TRUE)
+  } else {
+    return(FALSE)
+  }
 }
