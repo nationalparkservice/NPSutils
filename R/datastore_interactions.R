45b21fffdc8bf79f6b33970255ea12bec96d42c5
Rob Baker <robert_baker@nps.gov>
2024-05-29 12:11:24 -0600

    add dev parameter to all API calls

--- R/datastore_interactions.R
+++ R/datastore_interactions.R
@@ -6,7 +6,8 @@
 #' @description given a DataStore reference ID, the function will hit the DataStore API to determine whether the reference exists. If it cannot contact DataStore, it will stop with an error that states the connection failed. 
 #'
 #' @param reference_id Integer. A 7-digit number indicating the datastore reference to be queried.
-#' @param secure Logical. Defaults to TRUE, indicating that the user is logged on to the NPS VPN (or is accessing the resource from an NPS office). Can be set to FALSE for non-NPS users.
+#' @param secure Logical. Defaults to TRUE, indicating that the user is logged on to the NPS VPN (or is accessing the resource from an NPS office). Can be set to FALSE for non-NPS users. If `secure` is set to FALSE, `dev` is automatically set to FALSE.
+#' @param dev Logical. Defaults to FALSE, indicating that the user wishes to work with the production version of DataStore. Can be set to TRUE if the user wants to work with the development environment on DataStore. Setting dev = TRUE will only work if the user has the appropriate permissions. Requires secure = TRUE.
 #'
 #' @return logical. TRUE means the reference exists, false means it does not exist.
 #' @export
@@ -16,28 +17,28 @@
 #' check_ref_exists(1234567)
 #' check_ref_exists(1234567, secure = FALSE)
 #' }
-check_ref_exists <- function (reference_id, secure = TRUE) {
+check_ref_exists <- function (reference_id, secure = TRUE, dev = FALSE) {
   
   test_req <- NULL
   if (secure == FALSE) {
     #using secure api should be an option though...
     url <- paste0(.ds_api(), "ReferenceCodeSearch?q=", reference_id)
-    test_req <- httr::GET(url)
-    status_code <- httr::stop_for_status(test_req)$status_code
-    #if API call fails, alert user and remind them to log on to VPN:
-    if(!status_code == 200){
-      stop("DataStore connection failed. Try setting secure = TRUE and logging on to the VPN.\n")
-    }
   }
   if (secure == TRUE) {
-    url <- paste0(.ds_secure_api(), "ReferenceCodeSearch?q=", reference_id)
-    test_req <- httr::GET(url, httr::authenticate(":", ":", "ntlm"))
-    status_code <- httr::stop_for_status(test_req)$status_code
-    #if API call fails, alert user and remind them to log on to VPN:
-    if (!status_code == 200) {
-      stop("DataStore connection failed. Are you logged in to the VPN?\n")
+    if (dev == FALSE) {
+      url <- paste0(.ds_secure_api(), "ReferenceCodeSearch?q=", reference_id)
+    }
+    if (dev == TRUE) {
+      url <- paset0(.ds_dev_api(), "ReferenceCodeSearch?=", reference_id)
     }
   }
+  test_req <- httr::GET(url, httr::authenticate(":", ":", "ntlm"))
+  status_code <- httr::stop_for_status(test_req)$status_code
+  #if API call fails, alert user and remind them to log on to VPN:
+  if (!status_code == 200) {
+    stop("DataStore connection failed. Are you logged in to the VPN?\n")
+  }
+
   #get API request info:
   ref_data <- jsonlite::fromJSON(httr::content(test_req, "text"))
   
@@ -63,26 +64,30 @@ check_ref_exists <- function (reference_id, secure = TRUE) {
 #' check_is_data_package(1234567)
 #' check_is_data_package(1234567, secure = FALSE)
 #' }
-check_is_data_package <- function(reference_id, secure = TRUE) {
+check_is_data_package <- function(reference_id, secure = TRUE, dev = FALSE) {
+  test_req <- NULL
+  
   test_req <- NULL
   if (secure == FALSE) {
+    #using secure api should be an option though...
     url <- paste0(.ds_api(), "ReferenceCodeSearch?q=", reference_id)
-    test_req <- httr::GET(url)
-    status_code <- httr::stop_for_status(test_req)$status_code
-    #if API call fails, alert user and remind them to log on to VPN:
-    if(!status_code == 200){
-      stop("DataStore connection failed. Try setting secure = TRUE and logging on to the VPN.\n")
-    }
   }
   if (secure == TRUE) {
-    url <- paste0(.ds_secure_api(), "ReferenceCodeSearch?q=", reference_id)
-    test_req <- httr::GET(url, httr::authenticate(":", ":", "ntlm"))
-    status_code <- httr::stop_for_status(test_req)$status_code
-    #if API call fails, alert user and remind them to log on to VPN:
-    if (!status_code == 200) {
-      stop("DataStore connection failed. Are you logged in to the VPN?\n")
+    if (dev == FALSE) {
+      url <- paste0(.ds_secure_api(), "ReferenceCodeSearch?q=", reference_id)
+    }
+    if (dev == TRUE) {
+      url <- paset0(.ds_dev_api(), "ReferenceCodeSearch?=", reference_id)
     }
   }
+  
+  test_req <- httr::GET(url, httr::authenticate(":", ":", "ntlm"))
+  status_code <- httr::stop_for_status(test_req)$status_code
+  #if API call fails, alert user and remind them to log on to VPN:
+  if (!status_code == 200) {
+    stop("DataStore connection failed. Are you logged in to the VPN?\n")
+  }
+  
   #get reference info:
   ref_data <- jsonlite::fromJSON(httr::content(test_req, "text"))
   ref_type <- ref_data$referenceType
@@ -113,27 +118,30 @@ check_is_data_package <- function(reference_id, secure = TRUE) {
 #' check_new_version(1234567)
 #' check_new_version(1234567, secure = FALSE)
 #' }
-check_new_version <- function(referenc_id, secure = TRUE) {
+check_new_version <- function(reference_id, secure = TRUE, dev = FALSE) {
+  test_req <- NULL
+  
   test_req <- NULL
   if (secure == FALSE) {
     #using secure api should be an option though...
     url <- paste0(.ds_api(), "ReferenceCodeSearch?q=", reference_id)
-    test_req <- httr::GET(url)
-    status_code <- httr::stop_for_status(test_req)$status_code
-    #if API call fails, alert user and remind them to log on to VPN:
-    if(!status_code == 200){
-      stop("DataStore connection failed. Try setting secure = TRUE and logging on to the VPN.\n")
-    }
   }
   if (secure == TRUE) {
-    url <- paste0(.ds_secure_api(), "ReferenceCodeSearch?q=", reference_id)
-    test_req <- httr::GET(url, httr::authenticate(":", ":", "ntlm"))
-    status_code <- httr::stop_for_status(test_req)$status_code
-    #if API call fails, alert user and remind them to log on to VPN:
-    if (!status_code == 200) {
-      stop("DataStore connection failed. Are you logged in to the VPN?\n")
+    if (dev == FALSE) {
+      url <- paste0(.ds_secure_api(), "ReferenceCodeSearch?q=", reference_id)
+    }
+    if (dev == TRUE) {
+      url <- paset0(.ds_dev_api(), "ReferenceCodeSearch?=", reference_id)
     }
   }
+  
+  test_req <- httr::GET(url, httr::authenticate(":", ":", "ntlm"))
+  status_code <- httr::stop_for_status(test_req)$status_code
+  #if API call fails, alert user and remind them to log on to VPN:
+  if (!status_code == 200) {
+    stop("DataStore connection failed. Are you logged in to the VPN?\n")
+  }
+
   #get request info:
   ref_data <- jsonlite::fromJSON(httr::content(test_req, "text"))
   #check for a newer version:    
@@ -144,5 +152,56 @@ check_new_version <- function(referenc_id, secure = TRUE) {
     return(TRUE)
   } else {
     return(FALSE)
+  }
+}
+
+#' Get newest DataStore reference version
+#' 
+#' @description If a DataStore reference has been versioned, `get_new_version_id()` will return the DataStore reference ID for the newest version. If the reference has not been versioned, the function returns NULL.
+#'
+#' @inheritParams check_ref_exists 
+#'
+#' @return Integer. A 7-digit integer corresponding to the DataStore reference ID for the newest version of the supplied DataStore reference. If the supplied reference has not been versioned, NULL is returned.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' get_new_version_id(1234567)
+#' get_new_version_id(1234567, secure = FALSE)
+#' }
+get_new_version <- function(reference_id, secure = TRUE, dev = FALSE) {
+  #gets a newer version (assuming a newer version exists)
+  test_req <- NULL
+  
+  test_req <- NULL
+  if (secure == FALSE) {
+    #using secure api should be an option though...
+    url <- paste0(.ds_api(), "ReferenceCodeSearch?q=", reference_id)
+  }
+  if (secure == TRUE) {
+    if (dev == FALSE) {
+      url <- paste0(.ds_secure_api(), "ReferenceCodeSearch?q=", reference_id)
+    }
+    if (dev == TRUE) {
+      url <- paset0(.ds_dev_api(), "ReferenceCodeSearch?=", reference_id)
+    }
+  }
+  test_req <- httr::GET(url, httr::authenticate(":", ":", "ntlm"))
+  status_code <- httr::stop_for_status(test_req)$status_code
+  #if API call fails, alert user and remind them to log on to VPN:
+  if (!status_code == 200) {
+    stop("DataStore connection failed. Are you logged in to the VPN?\n")
+  }
+  
+  #get request info:
+  ref_data <- jsonlite::fromJSON(httr::content(test_req, "text"))
+  #check for a newer version:    
+  version <-ref_data$mostRecentVersion
+  
+  if (!is.na(version)) {
+    return(version)
+  }
+  if (is.na(version)) {
+    return(NULL)
   }
 }
