4a2867111224f6318e603febff25ef08aa5d2fa7
Rob Baker <robert_baker@nps.gov>
2024-08-26 13:10:01 -0600

    refactor for snake case; when dev=TRUE no longer need to also specify secure = TRUE

--- R/load_data_package.R
+++ R/load_data_package.R
@@ -1,8 +1,8 @@
-#' Read contents of data package file and constructs a list of tibbles based on the data file(s)
+#' Read contents of data package and constructs a list of tibbles based on the data file(s)
 #'
-#' @description \code{load_data_package} reads the data file(s) from a package and loads it into a list of tibbles. Current implementation only supports .csv data files. Planned future iterations will support .json and .xml data files.
+#' @description \code{load_data_package} reads the data file(s) from a package and loads it into a list of tibbles. Current implementation only supports .csv data files.
 #'
-#' @param holding_id is a 6-7 digit number corresponding to the holding ID of the data package.
+#' @param reference_id is a 6-7 digit number corresponding to the reference ID of the data package.
 #'
 #' @return a list of one or more tibbles contained within the data package to the global environment.
 #'
@@ -12,27 +12,27 @@
 #' \dontrun{
 #' load_data_package(2272461)
 #' }
-load_data_package <- function(holding_id) {
-  DataPackageDirectory <- paste("data/", holding_id, sep = "")
-  DataPackageFilename <- paste(DataPackageDirectory, ".zip", sep = "")
+load_data_package <- function(reference_id) {
+  data_package_directory <- paste("data/", reference_id, sep = "")
+  data_package_filename <- paste(data_package_directory, ".zip", sep = "")
 
   # Look for the zipped data package and attempt to unzip it. If the zipped file exists but cannot be unzipped, give the user a warning. If neither the unzipped nor zipped data packages exist, suggest the user check their working directory or use getDataPackage() to get the data package.
-  if (!file.exists(DataPackageDirectory)) {
-    if (file.exists(DataPackageFilename)) {
+  if (!file.exists(data_package_directory)) {
+    if (file.exists(data_package_filename)) {
       tryCatch(expr = {
-        fileList <- utils::unzip(DataPackageFilename, list = TRUE)
+        file_list <- utils::unzip(data_package_filename, list = TRUE)
       }, error = function(e) {
         message("The zip file cannot be unzipped.")
         stop()
       })
     }
-    if (!file.exists(DataPackageFilename)) {
+    if (!file.exists(data_package_filename)) {
       cat("The data package zip folder for Holding ",
-        crayon::blue$bold(holding_id), " was not found.\n",
+        crayon::blue$bold(reference_id), " was not found.\n",
         sep = ""
       )
       cat("Check to make sure you have the correct working directory.\n")
-      cat("Or try using ", crayon::green$bold("get_data_package()"), sep = "")
+      cat("Or try using ", crayon::green$bold("get_data_packages()"), sep = "")
     }
   }
 
@@ -46,22 +46,22 @@ load_data_package <- function(holding_id) {
 
   # if there is no "data" sub-folder, load all .csv files into tibbles and names each tibble based on the .csv file name (tibble to speed up processing).
   #### RLB: will need to be expanded to handle .xml and .json
-  if (!file.exists(paste0("/data/", holding_id, "/data"))) {
+  if (!file.exists(paste0("/data/", reference_id, "/data"))) {
     filenames <- list.files(
-      path = DataPackageDirectory,
+      path = data_package_directory,
       pattern = "*csv"
     )
     ## Create list of data frame names without the ".csv" part
     names <- gsub(pattern = "\\.csv$", "", filenames)
     ### Load all files into tibbles
-    tibble_List <- list()
+    tibble_list <- list()
     for (i in names) {
-      filepath <- file.path(DataPackageDirectory, paste(i, ".csv", sep = ""))
-      suppressWarnings(tibble_List[[i]] <- assign(i,
-                                                  readr::read_csv(filepath,
+      file_path <- file.path(data_package_directory, paste(i, ".csv", sep = ""))
+      suppressWarnings(tibble_list[[i]] <- assign(i,
+                                                  readr::read_csv(file_path,
                                                   show_col_types = FALSE)))
     }
-    return(tibble_List)
+    return(tibble_list)
   }
 
 
@@ -75,21 +75,21 @@ load_data_package <- function(holding_id) {
 
   # Looks for data within a "data" sub-folder for data packages (moslty generated after 3/2022). Loads all .csv files in the "data" sub-folder into tibbles (to speed up processing) and gives each tibble the filename of the .csv it came from (without .csv extension)
   #### RLB: will need to be expanded to handle .xml and .json
-  if (file.exists(paste0("data/", holding_id, "/data"))) {
+  if (file.exists(paste0("data/", reference_id, "/data"))) {
     ## change directories to the /data sub-directory within the data package
-    DataPackageDirectory <- paste0(DataPackageDirectory, "/data")
+    data_package_directory <- paste0(data_package_directory, "/data")
 
-    filenames <- list.files(path = DataPackageDirectory, pattern = "*csv")
+    filenames <- list.files(path = data_package_directory, pattern = "*csv")
 
     ## Create list of data frame names without the ".csv" part
     names <- gsub(pattern = "\\.csv$", "", filenames)
 
     ### Load all files
-    tibble_List <- list()
+    tibble_list <- list()
     for (i in names) {
-      filepath <- file.path(DataPackageDirectory, paste(i, ".csv", sep = ""))
-      tibble_List[[i]] <- assign(i, readr::read_csv(filepath, show_col_types = FALSE))
+      file_path <- file.path(data_package_directory, paste(i, ".csv", sep = ""))
+      tibble_list[[i]] <- assign(i, readr::read_csv(file_path, show_col_types = FALSE))
     }
-    return(tibble_List)
+    return(tibble_list)
   }
 }
