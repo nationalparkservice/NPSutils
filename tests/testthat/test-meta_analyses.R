3739df86f993fef12010143866d9a4dc0cad6a98
Robert Baker <robert_baker@nps.gov>
2025-05-08 20:58:34 -0600

    add unit test for get_refs_info and summarize_packages

--- tests/testthat/test-meta_analyses.R
+++ tests/testthat/test-meta_analyses.R
@@ -1,4 +1,5 @@
 
+# ---- get_ref_list ----
 httptest::with_mock_dir("get_ref_list_dir", {
   test_that("get_ref_list returns a vector of 7-digit reference IDs", {
     
@@ -20,5 +21,54 @@ httptest::with_mock_dir("get_ref_list_dir", {
     }
     expect_true(correct_length)
     
+  })  
+})
+
+# ---- get_refs_info ----
+
+httptest::with_mock_dir("get_refs_info_dir", {
+  test_that("get_refs_info returns expected values", {
+    #list of 5 test references (all data packages)
+    
+    x <- get_refs_info(reference_type = "dataPackage",
+                      no_of_entries = 5,
+                      secure = FALSE)
+    
+    #returns info no 5 references:
+    expect_equal(nrow(x), 5)
+    
+    #returns expected number of columns:
+    expect_equal(ncol(x), 14)
+    
+    #returns expected columns:
+    expect_equal(names(x), c("referenceId", "referenceType", "dateOfIssue",
+                             "visibility", "fileCount", "fileAccess",
+                             "title", "citation", "referenceUrl",
+                             "referenceGroupType", "typeName", "isDOI",
+                             "newVersion", "mostRecentVersion"))
+  })  
+})
+
+# ---- summarize_packages ----
+
+httptest::with_mock_dir("summarize_packages_dir", {
+  test_that("get_ref_info returns expected values", {
+    #list of 5 test references (all data packages)
+    x <- get_refs_info(reference_type = "dataPackage",
+                       no_of_entries = 5,
+                       secure = FALSE)
+    
+    #returns info no 5 references:
+    expect_equal(nrow(x), 5)
+    
+    #returns expected number of columns:
+    expect_equal(ncol(x), 14)
+    
+    #returns expected columns:
+    expect_equal(names(x), c("referenceId", "referenceType", "dateOfIssue",
+                             "visibility", "fileCount", "fileAccess",
+                             "title", "citation", "referenceUrl",
+                             "referenceGroupType", "typeName", "isDOI",
+                             "newVersion", "mostRecentVersion"))
   })  
 })
