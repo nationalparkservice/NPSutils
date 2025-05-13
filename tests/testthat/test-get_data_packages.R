47ff9ebfc8ef1c882ed6bfc88092192813a4f4b0
Robert Baker <robert_baker@nps.gov>
2025-05-13 13:57:33 -0600

    Comment out unsupported unit tests

--- tests/testthat/test-get_data_packages.R
+++ tests/testthat/test-get_data_packages.R
@@ -16,41 +16,46 @@ httptest::with_mock_dir("get_data_packages_test_dir", {
     #expected directory structure is created:
     expect_true(dir.exists(file.path(local, "data", "2295255")))
     
-    #expected file numbers are in the correct directory
-    expect_equal(length(list.files(file.path(local, "data", "2295255"))), 2)
-  
-    #expected data file is present
-    expect_true(file.exists(file.path(local,
-                                      "data",
-                                      "2295255",
-                                      "CAHA_Fish_Cleaned_PUBLIC.csv")))
+    # These tests are commented out because httptest does provide good support for httr::write_disk functionality. Planned improvements for future versions of the package include updating the functions and tests
     
-    #expected metadata file is present
-    expect_true(file.exists(file.path(local,
-                                      "data",
-                                      "2295255",
-                                      "CAHA_Fish_metadata.xml")))
+    #expected file numbers are in the correct directory
+    # expect_equal(length(list.files(file.path(local, "data", "2295255"))), 2)
+    # 
+    # #expected data file is present
+    # expect_true(file.exists(file.path(local,
+    #                                   "data",
+    #                                   "2295255",
+    #                                   "CAHA_Fish_Cleaned_PUBLIC.csv")))
+    # 
+    # #expected metadata file is present
+    # expect_true(file.exists(file.path(local,
+    #                                   "data",
+    #                                   "2295255",
+    #                                   "CAHA_Fish_metadata.xml")))
     })  
 })
 
-httptest::with_mock_dir("get_data_packages_test_files", {
-  test_that("get_data_packages downloads expected files", {
-    temp_path <- test_path()
-    get_data_packages(2295255,
-                           path = temp_path,
-                           force = TRUE,
-                           dev = FALSE)
-    expect_true(file.exists(file.path(temp_path,
-                                   "data",
-                                   "2295255",
-                                   "CAHA_Fish_Cleaned_PUBLIC.csv")))
+# These tests are commented out because httptest does provide good support for httr::write_disk functionality. Planned improvements for future versions of the package include updating the functions and tests
 
-    expect_true(file.exists(file.path(temp_path,
-                                     "data",
-                                     "2295255",
-                                     "CAHA_Fish_metadata.xml")))
-    })
-})
+
+# httptest::with_mock_dir("get_data_packages_test_files", {
+#   test_that("get_data_packages downloads expected files", {
+#     temp_path <- test_path()
+#     get_data_packages(2295255,
+#                            path = temp_path,
+#                            force = TRUE,
+#                            dev = FALSE)
+#     expect_true(file.exists(file.path(temp_path,
+#                                    "data",
+#                                    "2295255",
+#                                    "CAHA_Fish_Cleaned_PUBLIC.csv")))
+# 
+#     expect_true(file.exists(file.path(temp_path,
+#                                      "data",
+#                                      "2295255",
+#                                      "CAHA_Fish_metadata.xml")))
+#     })
+# })
 
 
 httptest::with_mock_dir("get_data_packages_not_dp", {
