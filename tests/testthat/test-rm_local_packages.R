58d3be14178470cb67a40fce544f9bad1cdf840e
Robert Baker <robert_baker@nps.gov>
2025-05-13 13:59:22 -0600

    update local to temp_dir

--- tests/testthat/test-rm_local_packages.R
+++ tests/testthat/test-rm_local_packages.R
@@ -1,18 +1,19 @@
 httptest::with_mock_dir("rm_local_packages_dir", {
   test_that("rm_local_packages removes local packages", {
     
-    local <- test_path()
+    temp_dir <- withr::local_tempdir()
+    #local <- test_path()
     get_data_packages(2295255,
-                      path = local,
+                      path = temp_dir,
                       force = TRUE,
                       dev = FALSE)
     
     rm_local_packages(2295255,
                       all = TRUE,
-                      path = local,
+                      path = temp_dir,
                       force = FALSE)
     
-    expect_true(!dir.exists(file.path(local, "data", "2295255")))
+    expect_true(!dir.exists(file.path(temp_dir, "data", "2295255")))
     
   })
 })
